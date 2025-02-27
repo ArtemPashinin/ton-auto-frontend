c7c477faf2de74b889d4a956d1dc1afb2509a173
devcosmos <va-litv@yandex.ru>
2025-02-27 14:47:31 +0300

    feat: update tab bar

--- src/components/TabBar/TabBar.tsx
+++ src/components/TabBar/TabBar.tsx
@@ -1,17 +1,15 @@
-import { Container, Row } from "react-bootstrap";
-import { appRoutesData } from "../../enums/app-routes";
-import TabBarButton from "../buttons/TabBarButton/TabBarButton";
-import style from './TabBar.module.css'
+import { appRoutesData } from '../../enums/app-routes';
+import TabBarButton from '../buttons/TabBarButton/TabBarButton';
+
+import style from './TabBar.module.css';
 
 const TabBar = () => {
   return (
-    <Container className={`fixed-bottom pb-4 px-3 pt-2 ${style.searchMenu}`}>
-      <Row>
-        {appRoutesData.map((appRouteData, index) => (
-          <TabBarButton key={index} {...appRouteData} />
-        ))}
-      </Row>
-    </Container>
+    <div className={`fixed-bottom p-2 pb-4 d-grid rounded-4 ${style.searchMenu}`}>
+      {appRoutesData.map((appRouteData, index) => (
+        <TabBarButton key={index} {...appRouteData} />
+      ))}
+    </div>
   );
 };
 
--- src/components/buttons/TabBarButton/TabBarButton.tsx
+++ src/components/buttons/TabBarButton/TabBarButton.tsx
@@ -1,16 +1,17 @@
-import { Col } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
-import style from "./TabBarButton.module.css";
-import { AppRouteData } from "../../../enums/app-routes";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AppRouteData } from "../../../enums/app-routes";
+
+import style from "./TabBarButton.module.css";
 
 const TabBarButton = ({ path, iconData, title, margin }: AppRouteData) => {
   const currentPath = useLocation().pathname;
   return (
-    <Col
+    <div
       className={`defaultText ${
         currentPath === path ? style.pressed : ""
-      } user-select-none pb-1 pt-2 rounded-3 px-0 d-flex flex-column justify-content-end`}
+      } user-select-none pb-1 pt-2 px-0 d-flex flex-column justify-content-end`}
     >
       <Link
         to={path}
@@ -19,7 +20,7 @@ const TabBarButton = ({ path, iconData, title, margin }: AppRouteData) => {
         <FontAwesomeIcon {...iconData} />
         <p className={`mt-1 ${style.buttonTitle} ${margin}`}>{title}</p>
       </Link>
-    </Col>
+    </div>
   );
 };
 
